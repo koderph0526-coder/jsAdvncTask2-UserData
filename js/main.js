330397cafe744c4b71e123712a4c7eaa004b47dd
koderph0526-coder <koderph0526@gmail.com>
2026-09-11 10:22:49 +0200

    JsStarted_commentsAndMinorCss

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 const mainContainer = document.querySelector("#mainCont");
 const inputForm = document.querySelector("#showInput");
 const showN = document.querySelector("#showName");
+const showImg = document.querySelector("#showImg");
+
+inputForm.addEventListener;
