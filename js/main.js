fbb4b7ab472ef37edff253c5dbf5f274a1bf2059
koderph0526-coder <koderph0526@gmail.com>
2026-09-13 14:05:20 +0200

    templateHtmlUp_JsFetchingInProg

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ const mainContainer = document.querySelector("#mainCont");
 const inputForm = document.querySelector("#showInput");
 const showN = document.querySelector("#showName");
 const showImg = document.querySelector("#showImg");
+const showLiNames = document.querySelector("#showsList");
 
-inputForm.addEventListener;
+inputForm.addEventListener("submit", (e) => {
+  e.preventDefault(); //preventing/removing standard browser settings
+});
